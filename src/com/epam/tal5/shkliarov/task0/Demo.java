57b3834faf6c5de980217862bb49506514520802
ashklyarovjr <anton.shklyarov@gmail.com>
2015-04-21 18:34:45 +0300

    Task 0

--- src/com/epam/tal5/shkliarov/task0/Demo.java
+++ src/com/epam/tal5/shkliarov/task0/Demo.java
@@ -5,6 +5,9 @@ package com.epam.tal5.shkliarov.task0;
  */
 public class Demo {
     public static void main(String[] args) {
-
+        Database database = new Database();
+        for (int i = 0; i < database.getAbiturients().length; i++) {
+            System.out.println(database.getAbiturients()[i].toString());
+        }
     }
 }
