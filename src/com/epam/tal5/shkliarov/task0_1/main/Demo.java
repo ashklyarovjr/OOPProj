bdee355a37ef38c22292cf5811b4b2d4d0858b56
ashklyarovjr <anton.shklyarov@gmail.com>
2015-04-23 17:35:50 +0300

    Task 0_1

--- src/com/epam/tal5/shkliarov/task0_1/main/Demo.java
+++ src/com/epam/tal5/shkliarov/task0_1/main/Demo.java
@@ -1,10 +1,27 @@
 package com.epam.tal5.shkliarov.task0_1.main;
 
+import com.epam.tal5.shkliarov.task0_1.recipes.Olivier;
+
 /**
  * Created by Anton_Shkliarov on 4/23/2015.
  */
 public class Demo {
     public static void main(String[] args) {
+        Olivier olivier = new Olivier();
+        olivier.makeOlivierSalad();
+        olivier.showRecipe();
+        System.out.println();
+
+        olivier.sortByCalories();
+        olivier.showRecipe();
+        System.out.println();
+
+        olivier.sortByWeight();
+        olivier.showRecipe();
+        System.out.println();
+
+        olivier.getVegsFromNeededCaloriesInterval(10, 200);
+        System.out.println();
 
     }
 }
